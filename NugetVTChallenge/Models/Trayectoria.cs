187f08fa2644babdb43b0958c0d6980ffe5f4d4a
David Domínguez Blanco <david.dominguez.blanco.2001@gmail.com>
2023-06-01 14:20:06 +0200

    trayectoria

---
+++ NugetVTChallenge/Models/Trayectoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetVTChallenge.Models {
+    public class Trayectoria {
+        public int Team { get; set; }
+        public int Tid { get; set; }
+        public string TournamentName { get; set; }
+        public string TournamentRank { get; set; }
+        public DateTime TournamentDateInit { get; set; }
+    }
+}
