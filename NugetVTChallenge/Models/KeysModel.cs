a5d70387effe8138dd0eee9ac123ce9f26e71ed6
andreiggar <andreiggar@gmail.com>
2023-06-12 09:44:52 +0200

    Nuget 7.0

---
+++ NugetVTChallenge/Models/KeysModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetVTChallenge.Models
+{
+    public class KeysModel
+    {
+        public string RDS { get; set; }
+        public string API { get; set; }
+        public string APILocal { get; set; }
+        public string BucketName { get; set; }
+        public string BucketUrl { get; set; }
+        public string EmailUser { get; set; }
+        public string EmailPassword { get; set; }
+        public string EmailServer { get; set; }
+        public string Email { get; set; }
+        public string Elasticache { get; set; }
+        public string ElasticacheName { get; set; }
+    }
+}
